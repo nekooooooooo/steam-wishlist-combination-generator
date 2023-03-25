4edb8082939c5a41b204f759436b9df3917b034d
nekooooooooo <nekowalkthrough@gmail.com>
2023-03-25 22:18:14 +0800

    Add steam id/steam url functionality

---
+++ utils/constants.py
@@ -0,0 +1,2 @@
+STEAM_WISHLIST_VANITY_URL = "https://store.steampowered.com/wishlist/id/{}/wishlistdata?p={}"
+STEAM_WISHLIST_ID_URL = "https://store.steampowered.com/wishlist/profiles/{}/wishlistdata?p={}"
