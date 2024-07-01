589ba811b0a3377edb70504cf24b60189620d618
nekooooooooo <nekowalkthrough@gmail.com>
2024-07-02 02:12:14 +0800

    Remove default currency string

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,3 @@
 STEAM_WISHLIST_VANITY_URL = "https://store.steampowered.com/wishlist/id/{}/wishlistdata?p={}"
 STEAM_WISHLIST_ID_URL = "https://store.steampowered.com/wishlist/profiles/{}/wishlistdata?p={}"
-# CURRENCY = "PHP"
+CURRENCY = ""
